476f41d754912f4a251e2479f76465a1482a7cc1
Ana Ea <aabad0464@gmail.com>
2021-08-04 04:17:35 -0600

    use  routes in server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
