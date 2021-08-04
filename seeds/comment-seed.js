b10ba7b85d8a3f9a5430fe8d63116d739eb93c7f
Ana Ea <aabad0464@gmail.com>
2021-08-04 03:47:24 -0600

    updated commentSeed comments

--- seeds/comment-seed.js
+++ seeds/comment-seed.js
@@ -2,27 +2,27 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        comment_text:'',
+        comment_text:'That is very useful to know',
         post_id: 1,
         user_id: 3,
     },
     {
-        comment_text:'',
+        comment_text:'Great to know, thank you',
         post_id: 2,
         user_id: 5,
     },
     {
-        comment_text:'',
+        comment_text:'Very useful!',
         post_id: 3,
         user_id: 4,
     },
     {
-        comment_text:'',
+        comment_text:'Would make sure to keep that in mind',
         post_id: 4,
         user_id: 2,
     },
     {
-        comment_text:'',
+        comment_text:'Thank you for the tip!',
         post_id: 5,
         user_id: 1,
     },
