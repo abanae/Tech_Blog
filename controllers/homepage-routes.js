f162ca7c3c230198c80161c75a1c80a0eed5f4a5
Ana Ea <aabad0464@gmail.com>
2021-08-06 06:57:23 -0600

    user routes test

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -6,18 +6,13 @@ const { Comment, Post, User } = require('../models');
 // Getting all Posts
 router.get('/', async (req, res) => {
     try {
-        const posts = await Post.findAll({ 
+        const allPosts = await Post.findAll({ 
             include:[Comment,{ model: User} ],
         });
-        const tempArr = [
-            {
-                title: 'whatever',
-            },
-            {
-                title: 'something else',
-            }
-        ];
-        res.render("homepage", {tempArr});
+        const posts = allPosts.map(post => post.toJSON());
+        const allUsers = await User.findAll();
+        const users = allUsers.map(user => user.toJSON());
+        res.render("homepage", {posts, users});
     } catch (err) {
         res.status(500).json(err);
     }
