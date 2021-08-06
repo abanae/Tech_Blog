8d34e456256cd2db45b99dc89782c79d8fe9d9b6
Ana Ea <aabad0464@gmail.com>
2021-08-06 15:37:52 -0600

    setting hooks for users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,16 +2,16 @@
 const router = require('express').Router();
 const { Comment, Post ,User } = require('../../models');
 
-// get all customers route
-router.get('/', async (req, res) => {
-    try {
-        const userData = await User.findAll({
-            attributes: { exclude: ['password'] }
-            });
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+
+// router.get('/', async (req, res) => {
+//     try {
+//         const userData = await User.findAll({
+//             attributes: { exclude: ['password'] }
+//             });
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
