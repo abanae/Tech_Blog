102c3f34e148d1174c25a096ec1405df0ad49f70
Ana Ea <aabad0464@gmail.com>
2021-08-06 14:16:01 -0600

    testing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,16 @@
 // Dependencies
 const router = require('express').Router();
-// const { User } = require('../../models');
+const { User } = require('../../models');
 
 // get all customers route
 router.get('/', async (req, res) => {
     try {
-        const users = await User.findAll().map(user => user.toJSON());
+        const allUsers = await User.findAll();
+        const users = allUsers.map(user => user.toJSON());
         // render to homepage
         res.status(200).render('homepage', { users });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
