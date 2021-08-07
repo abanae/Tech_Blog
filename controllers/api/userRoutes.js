94f6ca1d38ab15d654653839ff5848a5a2dc0888
Ana Ea <aabad0464@gmail.com>
2021-08-06 23:02:57 -0600

    setting route to log in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,5 @@
 // Dependencies
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
 const { Comment, Post, User } = require('../../models');
 
 // Creating a new user account
@@ -12,6 +11,34 @@ router.post('/', async (req, res) => {
             req.session.email = userData.email;
             req.session.logged_in = true;
             res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
         });
     } catch (err) {
         res.status(400).json(err);
