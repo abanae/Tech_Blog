63119cfb98f5c9a9b6adb4fc38782b6b981dd215
Ana Ea <aabad0464@gmail.com>
2021-08-07 00:29:51 -0600

    new comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,6 +4,25 @@ const withAuth = require('../../utils/auth');
 const { Comment } = require('../../models');
 
 
+// Getting all comments
+// router.post('/', (req, res) => {
+// });
+
+
+// New Comment
+router.post('/', withAuth, (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 
