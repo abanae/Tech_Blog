bdd938ece434eb74bade3124c7f9357fe93bbcbe
Ana Ea <aabad0464@gmail.com>
2021-08-06 22:06:09 -0600

    creating new users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,23 +1,22 @@
 // Dependencies
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
-const { Comment, Post ,User } = require('../../models');
+const { Comment, Post, User } = require('../../models');
 
+// Creating a new user account
 router.post('/', async (req, res) => {
     try {
-      const userData = await User.create(req.body);
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.username = userData.username;
-        req.session.email = userData.email;
-        req.session.password = userData.password;
-        req.session.logged_in = true;
-        res.status(200).json(userData);
-      });
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.username = userData.username;
+            req.session.email = userData.email;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  });
+});
 
 
 module.exports = router;
