0b97afb744c110965fa179b1cd535ad464731df7
Ana Ea <aabad0464@gmail.com>
2021-08-07 03:09:59 -0600

    dashboard javascript

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,51 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // QuerySelectors
+    const name = document.querySelector('#post-name').value.trim();
+    // const needed_funding = document.querySelector('#project-funding').value.trim();
+    const description = document.querySelector('#post-desc').value.trim();
+  
+    // Create Posts 
+    if (name && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  // Delete Posts
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete posts');
+      }
+    }
+  };
+  
+  // Bttms Query Selectors
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
