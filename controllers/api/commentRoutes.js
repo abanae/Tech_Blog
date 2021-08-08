3db4809cf905ffa0f50f4746ea358f58dc23a83a
Ana Ea <aabad0464@gmail.com>
2021-08-08 09:00:01 -0600

    testing getting a single post

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,7 +15,6 @@ router.get('/', async (req, res) => {
         }
 });
 
-
 // New Comment
 router.post('/', withAuth, async (req, res) => {
     try {
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,9 +16,23 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+// getting a single post
+router.get("/:id", withAuth, async (req, res) =>{
+    try {
+        const postSingle = await Post.findByPk(req.params.id, {
+            include: [User, { model: Comment }]
+        });
+        if (!postSingle) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json();
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-
-// delete blog post
+// delete post
 router.delete('/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.destroy({
