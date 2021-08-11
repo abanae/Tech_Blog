47cfa529a1346b62b9d0d6c77c613e708bfaf4ae
Ana Ea <aabad0464@gmail.com>
2021-08-11 01:52:43 -0600

    debugging

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -37,6 +37,48 @@ router.get('/getpost/:id', withAuth, async (req, res) =>{
     }
 });
 
+// Update post
+router.put('/:id',withAuth, async (req, res) => {
+    try {
+        console.log(req.body)
+      const postUpdate = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+        }
+  
+      });
+      if (!postUpdate[0]) {
+        res.status(404).json({ message: 'No post found with this id!' })
+        return;
+      }
+      res.status(200).json(postUpdate);
+  
+    } catch (err) {
+        console.log(err)
+      res.status(500).json(err);
+    }
+});
+
+router.get('/update/:id', withAuth, async (req, res) =>{
+    console.log('route hit');
+    try {
+        const postSingle = await Post.findByPk(req.params.id, {
+            include: [{
+                model: Comment,
+                include: [User]
+            }, User]
+        });
+        if (!postSingle) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        const single = postSingle.toJSON();
+        res.status(200).render('update',{...single,logged_in: req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); 
+
 // delete post
 router.delete('/:id', withAuth, async (req, res) => {
     try {
