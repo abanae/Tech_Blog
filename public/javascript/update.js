33e210b6e48ad718a4993b8cdbf729322119a9a0
Ana Ea <aabad0464@gmail.com>
2021-08-11 03:07:32 -0600

    update post completed

--- public/javascript/update.js
+++ public/javascript/update.js
@@ -3,11 +3,11 @@ const editHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
       const title = document.querySelector('#title').value.trim();
-      const content = document.querySelector('#content').value.trim();
+      const post_text = document.querySelector('#content').value.trim();
       
       const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
-        body: JSON.stringify({ title, content }),
+        body: JSON.stringify({ title, post_text}),
         headers: {
           'Content-Type': 'application/json',
         },
